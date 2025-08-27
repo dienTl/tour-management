44962477140b6da78fa42b2238d719a44830e680
buiminhdien <dienbui2005@gmail.com>
2025-08-27 20:53:59 +0700

    tạo controller

---
+++ controllers/client/tour.controllers.ts
@@ -0,0 +1,18 @@
+import Tour from "../../models/tour.model";
+import { Request, Response} from "express"
+
+// [GET] /tours/
+export const index =  async (req:Request,res:Response) =>{
+  // SELECT * from  tours where deleted =false and status="active"
+  const tours = await Tour.findAll({
+    where:{
+      deleted:false,
+      status:"active"
+    },
+    raw:true
+  });
+  console.log(tours)
+  res.render("client/pages/tours/index",{
+    tours: tours
+  })
+}
--- routes/client/tour.router.ts
+++ routes/client/tour.router.ts
@@ -1,17 +1,9 @@
 import {Router} from "express"
-import express, {Express ,Request, Response} from "express"
-import Tour from "../../models/tour.model";
 
 const route : Router = Router()
 
-route.get("/", async (req:Request,res:Response) =>{
-  const tours = await Tour.findAll({
-    raw:true
-  });
-  console.log(tours)
-  res.render("client/pages/tours/index",{
-    tours: tours
-  })
-})
+import * as controller from "../../controllers/client/tour.controllers"
+
+route.get("/",controller.index)
 
 export const tourRoutes : Router = route
